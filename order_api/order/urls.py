9e8492e8e28227700fb56619c214bb7813f5de45
Tettey1 <tetteynathan89@gmail.com>
2024-07-17 12:13:19 +0000

    modified:   order_api/order/urls.py

--- order_api/order/urls.py
+++ order_api/order/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', RootAPIView.as_view(), name='root-api'),
     path('orders', OrderCreateView.as_view(), name='order-create'),
     path('orders/<int:id>', OrderDetailView.as_view(), name='order-detail'),
-    path('users/<int:userId>/orders', UserOrdersListView.as_view(), name='user-orders-list'),
+    path('users/<int:userId>/orders', UserOrdersListView.as_view(), name='user-orders'),
 ]
